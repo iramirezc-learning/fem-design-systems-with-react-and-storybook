2dcd99a91361399db3456f46702e8a13900fa77e
Isaac Ramirez <iramirezc@live.com.mx>
2021-04-20 03:04:37 -0500

    Developing Styled Components: - solved Button activity exercise - add SecondaryButton - add TertiaryButton

--- packages/my-component-library/src/components/Buttons.js
+++ packages/my-component-library/src/components/Buttons.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
 
+const textColor = '#181818'
 const textInvertedColor = '#ffffff'
+const neutral100 = '#ffffff'
+const primary200 = '#8CDE59'
 const primary300 = '#6BDE23'
 
 const Button = styled.button`
+  background-color: ${neutral100};
   padding: 12px 24px;
   font-size: 1rem;
   border-radius: 5px;
@@ -12,10 +16,23 @@ const Button = styled.button`
   font-family: 'Noto Sans', monospace;
 `
 
-const PrimaryButton = styled(Button)`
+export const PrimaryButton = styled(Button)`
   background-color: ${primary300};
   border: none;
   color: ${textInvertedColor};
+`
+
+export const SecondaryButton = styled(Button)`
+  border-width: 3px;
+  border-color: ${primary200};
+  border-style: solid;
+  color: ${textColor};
+`
+
+export const TertiaryButton = styled(Button)`
+  border: none;
+  color: ${textColor};
+  box-shadow: 1px 2px 5px -2px rgba(0, 0, 0, 0.5);
 `
 
 export default PrimaryButton
--- packages/my-component-library/src/index.js
+++ packages/my-component-library/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimaryButton from './components/Buttons'
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from './components/Buttons'
 
 const App = () => (
   <>
     <h1>Hello World</h1>
     <PrimaryButton>Primary Button</PrimaryButton>
+    <br />
+    <br />
+    <SecondaryButton>Secondary Button</SecondaryButton>
+    <br />
+    <br />
+    <TertiaryButton>Tertiary Button</TertiaryButton>
   </>
 )
 
