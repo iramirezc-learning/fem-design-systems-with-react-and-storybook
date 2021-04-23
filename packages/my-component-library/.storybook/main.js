ed92675609e6ce77dff404a7f11322524c8b591a
Isaac Ramirez <iramirezc@live.com.mx>
2021-04-23 03:02:38 -0500

    Documenting Components with Storybook: - add Buttons stories - install addon-backgrounds - install addon-contexts

--- packages/my-component-library/.storybook/main.js
+++ packages/my-component-library/.storybook/main.js
@@ -4,6 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
+    '@storybook/addon-contexts/register',
+    '@storybook/addon-backgrounds/register',
     '@storybook/addon-docs',
   ],
 }
--- packages/my-component-library/.storybook/preview.js
+++ packages/my-component-library/.storybook/preview.js
@@ -1,10 +1,16 @@
+import { addParameters, addDecorator } from '@storybook/react'
+import { withContexts } from '@storybook/addon-contexts/react'
+import { contexts } from './contexts'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+// TODO: for some reason the story is not changing the background
+addParameters({
+  backgrounds: {
+    default: 'white',
+    values: [
+      { name: 'white', value: '#ffffff' },
+      { name: 'black', value: '#050449' },
+    ],
   },
-}
+})
+
+addDecorator(withContexts(contexts))
